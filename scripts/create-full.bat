1ed196f03f456869e392b75acac434f0911ac9c2
Wolfgang Wiese <xwolf@xwolf.de>
2018-08-21 09:56:17 +0200

    Windows Batch-Script

---
+++ scripts/create-full.bat
@@ -0,0 +1,16 @@
+@echo off
+cd ../docs/
+echo Markdown...
+pandoc -o ../export/leitfaden-barrierefreiheit.md ../metadata/metadata.yaml 01-vorwort.md 02-toc.md 03-inhalte.md 04-entwicklung.md 05-recht.md 06-satzungen.md 07-referenzen.md
+
+echo Word Docx...
+pandoc -o ../export/leitfaden-barrierefreiheit.docx ../metadata/metadata.yaml 01-vorwort.md 02-toc.md 03-inhalte.md 04-entwicklung.md 05-recht.md 06-satzungen.md 07-referenzen.md
+
+
+:: requires pdflatex:
+:: echo PDF...
+:: pandoc -o ../export/leitfaden-barrierefreiheit.pdf ../metadata/metadata.yaml 01-vorwort.md 02-toc.md 03-inhalte.md 04-entwicklung.md 05-recht.md 06-satzungen.md 07-referenzen.md
+
+echo EPub...
+pandoc -o ../export/leitfaden-barrierefreiheit.epub ../metadata/metadata.yaml 01-vorwort.md 02-toc.md 03-inhalte.md 04-entwicklung.md 05-recht.md 06-satzungen.md 07-referenzen.md
+cd ../scripts/
