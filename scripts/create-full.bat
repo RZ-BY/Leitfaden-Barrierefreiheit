e8fa039a6628b96e1a6d016fdd0113f351ba847e
xwolfde <xwolf@xwolf.de>
2018-09-09 11:25:23 +0200

    Inhaltsüberblick nach Vorwort
    Einführung nach TOC

--- scripts/create-full.bat
+++ scripts/create-full.bat
@@ -1,8 +1,8 @@
 @echo off
 cd ../docs/
-pandoc -o ../export/leitfaden-barrierefreiheit.docx ../metadata/metadata.yaml 01-vorwort.md 02-toc.md 03-inhalte.md 03-inhalte-spickzettel.md 04-entwicklung.md 04-browser-add-ons.md 05-recht.md 06-satzungen.md 07-tests.md 08-referenzen.md 09-impressum.md --reference-doc=../templates/custom-reference.docx
+pandoc -o ../export/leitfaden-barrierefreiheit.docx ../metadata/metadata.yaml 01-vorwort.md 01-toc.md 02-einfuehrung.md 03-inhalte.md 03-inhalte-spickzettel.md 04-entwicklung.md 04-browser-add-ons.md 05-recht.md 06-satzungen.md 07-tests.md 08-referenzen.md 09-impressum.md --reference-doc=../templates/custom-reference.docx
 
-pandoc --pdf-engine=xelatex -o ../export/leitfaden-barrierefreiheit.epub ../metadata/metadata.yaml 01-vorwort.md 02-toc.md 03-inhalte.md  03-inhalte-spickzettel.md 04-entwicklung.md 04-browser-add-ons.md 05-recht.md 06-satzungen.md 07-tests.md 08-referenzen.md 09-impressum.md 
+pandoc --pdf-engine=xelatex -o ../export/leitfaden-barrierefreiheit.epub ../metadata/metadata.yaml 01-vorwort.md 01-toc.md 02-einfuehrung.md 03-inhalte.md  03-inhalte-spickzettel.md 04-entwicklung.md 04-browser-add-ons.md 05-recht.md 06-satzungen.md 07-tests.md 08-referenzen.md 09-impressum.md 
 
 
 
